167047a6235382e037fb592645143816eb2e59f7
Oleh Shyian <shiyan4242@gmail.com>
2025-03-27 14:26:51 +0100

    1.1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React from "react";
 import Header from "./components/Header";
 import Users from "./components/Users";
+import AddUser from "./components/AddUser";
 
   
   class App extends React.Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        users: [
+          {
+              id: 1,
+              firstName: 'Alex',
+              lastName: 'Yannes',
+              bio: 'Bloger, youtuber',
+              age: 24,
+              isHappy: true
+          },
+          {
+              id: 2,
+              firstName: 'Dima',
+              lastName: 'Tretyakov',
+              bio: 'Moishik, citizen',
+              age: 42,
+              isHappy: false 
+          }
+      ]
+      }
+      this.addUser = this.addUser.bind(this)
+    }
+
     render () {
       return (
       <div >
         <Header title="Список пользователей"/>
         <main>
-          <Users />
+          <Users users={this.state.users} />
         </main>
-        <aside></aside>
+        <aside>
+          <AddUser onAdd={this.addUser}/>
+        </aside>
     </div>
     )
+    }
+
+    addUser(user) {
+      const id = this.state.users.length + 1
+      this.setState({ users: [...this.state.users, {id, ...user} ]})
     }
   }
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -2,34 +2,12 @@ import React from "react"
 import User from "./User"
 
 class Users extends React.Component {
-    constructor(props) {
-      super(props)
-      this.state = {
-        users: [
-          {
-              id: 1,
-              firstName: 'Alex',
-              lastName: 'Yannes',
-              bio: 'Bloger, youtuber',
-              age: 24,
-              isHappy: true
-          },
-          {
-              id: 2,
-              firstName: 'Dima',
-              lastName: 'Tretyakov',
-              bio: 'Moishik, citizen',
-              age: 42,
-              isHappy: false 
-          }
-      ]
-      }
-    }
+    
     render() {
-        if(this.state.users.length > 0)
+        if(this.props.users.length > 0)
       return (
         <div>
-            {this.state.users.map((el) => (
+            {this.props.users.map((el) => (
               <User key={el.id} user={el}/>
             ))}
         </div>
@@ -41,6 +19,10 @@ class Users extends React.Component {
         </div>
       )
     }
+
+   
+
+
   }
 
   export default Users
