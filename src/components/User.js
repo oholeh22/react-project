c378b419baa81d770c82766f569a003133935dc6
Oleh Shyian <shiyan4242@gmail.com>
2025-04-09 14:18:30 +0200

    editor

--- src/components/User.js
+++ src/components/User.js
@@ -1,16 +1,29 @@
 import React from "react"
+import AddUser from "./AddUser";
 import { IoCloseCircleSharp, IoHammerSharp } from "react-icons/io5";
 
 class User extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editForm: false
+    }
+  }
     render() {
         const { user } = this.props;
         return (
           <div className="user">
             <IoCloseCircleSharp onClick={() => this.props.onDelete(user.id)} className="delete-icon"/>
-            <IoHammerSharp className="edit-icon"/>
+            <IoHammerSharp onClick={() =>
+              this.setState({
+                editForm: !this.state.editForm
+              })
+             } className="edit-icon"/>
             <h3>{user.firstName} {user.lastName}</h3>
             <p>{user.bio}</p>
             <b>{user.isHappy ? 'Счастлив :)' : 'Не особо :('}</b>
+
+            {this.state.editForm && <AddUser />}
           </div>
         );
       }
